71399e05500e6de413e3bed5e1a0683999919aaf
Shuhaib <shuhaib.2019678@iit.ac.lk>
2022-06-18 15:22:30 +0530

    added backend

---
+++ models/Product.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema(
+  {
+    productName: { type: String, required: true, unique: true },
+    quantity: { type: Number, required: true },
+    productDescription: { type: String, required: true },
+    images: { type: Array, required: true },
+    sku: { type: Number, required: true },
+    price: { type: Number, required: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", ProductSchema);
